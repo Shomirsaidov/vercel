27a4b405f832c4ce7769e6c17a1c9740b3ef54b6
Shomirsaidov <115943155+Shomirsaidov@users.noreply.github.com>
2023-06-04 10:50:37 +0300

    Update index.js

--- index.js
+++ index.js
@@ -5,4 +5,8 @@ app.get('/api', (req,res) => {
     res.json({message: 'ALLAHU AKBAR !'})
 })
 
+app.get('/api/:message', (req,res) => {
+    res.json({message: req.params['message']})
+})
+
 app.listen(4444, () => console.log('Server started successfully !'))
